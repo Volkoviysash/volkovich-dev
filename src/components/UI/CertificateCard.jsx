403a55d80451298b19b3fdf0b50dcb88b315c31a
volkovich <volkoviysash@gmail.com>
2023-07-05 00:28:33 +0300

    fix: Fix color of the icons.

--- src/components/UI/CertificateCard.jsx
+++ src/components/UI/CertificateCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function CertificateCard({ title, description, image, certificateURL }) {
   return (
     <a href={certificateURL} target='_blank' rel='noreferrer'>
-      <div className='m-2 rounded-lg shadow-md h-full p-6 transform transition-all hover:scale-105 cursor-pointer dark:bg-blue-gray-800 dark:text-gray-300 bg-white'>
+      <div className='m-2 rounded-lg shadow-md h-full p-6 transform transition-all hover:scale-105 cursor-pointer dark:bg-blue-gray-800 dark:text-gray-400 bg-white'>
         <h3 className='text-xl font-medium mb-4'>{title}</h3>
         <div>
           <img
@@ -11,7 +11,7 @@ function CertificateCard({ title, description, image, certificateURL }) {
             alt='certificate'
             className='w-full h-96 rounded-lg'
           />
-          <p className='mt-4 mb-2 dark:text-gray-300 text-gray-600'>
+          <p className='mt-4 mb-2 dark:text-gray-400 text-gray-600'>
             {description}
           </p>
         </div>

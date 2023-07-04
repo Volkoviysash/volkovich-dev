89eca40e71fb544b83ec31fb6fd5fd5bd885299e
volkovich <volkoviysash@gmail.com>
2023-07-04 22:20:32 +0300

    fix: Add links.

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -11,35 +11,35 @@ function ContactMe() {
   return (
     <div className='h-20 text-5xl flex justify-center gap-4 md:gap-16 pt-3 text-gray-600 items-center'>
       <a
-        href='https://twitter.com'
+        href='https://twitter.com/TankerWolf'
         target='_blank'
         className='transition ease-in-out hover:scale-110 duration-300'
       >
         <AiFillTwitterCircle className='hover:text-teal-500 hover:ease-in' />
       </a>
       <a
-        href='https://twitter.com'
+        href='https://www.linkedin.com/in/alexandr-volcovici-13981a254?trk=contact-info'
         target='_blank'
         className='transition ease-in-out hover:scale-110 duration-300'
       >
         <AiFillLinkedin className='hover:text-teal-500' />
       </a>
       <a
-        href='https://twitter.com'
+        href='https://github.com/Volkoviysash'
         target='_blank'
         className='transition ease-in-out hover:scale-110 duration-300'
       >
         <AiFillGithub className='hover:text-teal-500' />
       </a>
       <a
-        href='https://twitter.com'
+        href='https://instagram.com/volkovich.a'
         target='_blank'
         className='transition ease-in-out hover:scale-110 duration-300'
       >
         <AiFillInstagram className='hover:text-teal-500' />
       </a>
       <a
-        href='https://twitter.com'
+        href='mailto:volkoviysash@gmail.com'
         target='_blank'
         className='transition ease-in-out hover:scale-110 duration-300'
       >
